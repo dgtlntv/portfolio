64dabba5c2394cc1a9efbf9a1577d548ec6598e8
dgtlntv <68336485+dgtlntv@users.noreply.github.com>
2022-08-02 01:45:41 +0200

    fixed mobile navigation

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -8,18 +8,20 @@ export default function Navigation() {
     return (
         <Popover className="relative bg-white z-10">
             <div className="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start">
+                <div className="flex justify-start lg:w-0 lg:flex-1">
+                    <a href="#">
+                        <Link to="/" className="text-xl font-semibold text-gray-900 hover:text-gray-500">
+                            mblazek
+                        </Link>
+                    </a>
+                </div>
                 <div className="-mr-2 -my-2 md:hidden">
                     <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black">
                         <span className="sr-only">Open menu</span>
                         <MenuIcon className="h-6 w-6" aria-hidden="true" />
                     </Popover.Button>
                 </div>
-                <div className="hidden md:flex-1 md:flex md:items-center md:justify-between">
-                    <div>
-                        <Link to="/" className="text-xl font-semibold text-gray-900 hover:text-gray-500">
-                            mblazek
-                        </Link>
-                    </div>
+                <div className="hidden md:flex-1 md:flex items-center justify-end lg:w-0">
                     <div className="flex items-center gap-8 md:mr-4">
                         <Link to="/portfolio" className="text-base font-medium text-gray-500 hover:text-gray-900 hover:underline hover:decoration-1 hover:underline-offset-8 hover:decoration-dashed">
                             Portfolio
@@ -50,6 +52,9 @@ export default function Navigation() {
                     <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
                         <div className="pt-5 pb-6 px-5">
                             <div className="flex items-center justify-between">
+                                <Link to="/" className="text-xl font-semibold text-gray-900 hover:text-gray-500">
+                                    mblazek
+                                </Link>
                                 <div className="-mr-2">
                                     <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black">
                                         <span className="sr-only">Close menu</span>
