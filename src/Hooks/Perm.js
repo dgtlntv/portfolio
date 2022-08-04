2cd43a97a30d464f54bc6c235a5b606cacb51b0e
dgtlntv <68336485+dgtlntv@users.noreply.github.com>
2022-08-04 16:56:57 +0200

    experimental fix for device orientation on safari

---
+++ src/Hooks/Perm.js
@@ -0,0 +1,71 @@
+import { isMobileSafari } from "react-device-detect"
+import Modal from "../Components/Modal"
+import { ReactDOM } from "react"
+
+export default async function Perm() {
+    if (!isMobileSafari) {
+        return await deviceOrientationPermission()
+    } else {
+        return await getPermissionFromModal()
+    }
+}
+
+function getPermissionFromModal() {
+    return new Promise((resolve, reject) => {
+        addDialog(resolve)
+    })
+}
+
+function addDialog(resolve) {
+    const body = document.getElementsByTagName("body")[0]
+    const div = document.createElement("div")
+    div.setAttribute("id", "DialogDiv")
+    body.appendChild(div)
+    ReactDOM.render(<Modal action={deviceOrientationPermission} resolve={resolve} remove={removeDialog} />, div)
+}
+
+function removeDialog() {
+    const div = document.getElementById("DialogDiv")
+    const body = document.getElementsByTagName("body")[0]
+    body.removeChild(div)
+}
+
+async function deviceOrientationPermission() {
+    return new Promise((resolve, reject) => {
+        if (navigator.permissions) {
+            Promise.all([navigator.permissions.query({ name: "accelerometer" }), navigator.permissions.query({ name: "gyroscope" })])
+                .then((results) => {
+                    if (results.every((result) => result.state === "granted")) {
+                        resolve(true)
+                    } else {
+                        resolve(false)
+                    }
+                })
+                .catch(() => {
+                    try {
+                        DeviceOrientationEvent.requestPermission().then(function (response) {
+                            if (response === "granted") {
+                                resolve(true)
+                            } else {
+                                resolve(false)
+                            }
+                        })
+                    } catch (err) {
+                        resolve(false)
+                    }
+                })
+        } else {
+            try {
+                DeviceOrientationEvent.requestPermission().then(function (response) {
+                    if (response === "granted") {
+                        resolve(true)
+                    } else {
+                        resolve(false)
+                    }
+                })
+            } catch {
+                resolve(false)
+            }
+        }
+    })
+}
--- src/Hooks/useDeviceOrientation.js
+++ src/Hooks/useDeviceOrientation.js
@@ -2,6 +2,7 @@ import { RelativeOrientationSensor } from "motion-sensors-polyfill"
 import { useRef, useEffect } from "react"
 import { Quaternion } from "three"
 import isEqual from "lodash.isequal"
+import Perm from "./Perm"
 
 export default function useDeviceOrientation() {
     const orientation = useRef({
@@ -13,63 +14,30 @@ export default function useDeviceOrientation() {
     }).current
 
     useEffect(function () {
-        let sensor = null
-        try {
-            sensor = new RelativeOrientationSensor({ frequency: 60, referenceFrame: "device" })
-            sensor.addEventListener("error", (event) => {
-                // Handle runtime errors.
-                if (event.error.name === "NotAllowedError") {
-                    if (navigator.permissions) {
-                        Promise.all([navigator.permissions.query({ name: "accelerometer" }), navigator.permissions.query({ name: "gyroscope" })])
-                            .then((results) => {
-                                if (results.every((result) => result.state === "granted")) {
-                                    sensor.addEventListener("reading", () => initSensor(orientation, sensor, initialOrientation))
-                                    sensor.start()
-                                } else {
-                                    console.log("No permissions to use AbsoluteOrientationSensor.")
-                                }
-                            })
-                            .catch((err) => {
-                                try {
-                                    DeviceOrientationEvent.requestPermission().then(function (response) {
-                                        if (response === "granted") {
-                                            sensor.addEventListener("reading", () => initSensor(orientation, sensor, initialOrientation))
-                                            sensor.start()
-                                        }
-                                    })
-                                } catch (err) {
-                                    console.error(err)
-                                }
-                            })
+        const sensor = new RelativeOrientationSensor({ frequency: 60, referenceFrame: "device" })
+
+        Perm().then(function (permission) {
+            if (permission) {
+                try {
+                    sensor.addEventListener("error", (event) => {
+                        console.log(event.error)
+                    })
+                    sensor.addEventListener("reading", () => initSensor(orientation, sensor, initialOrientation))
+                    sensor.start()
+                } catch (error) {
+                    if (error.name === "SecurityError") {
+                        console.log("Sensor construction was blocked by a feature policy.")
+                    } else if (error.name === "ReferenceError") {
+                        console.log("Sensor is not supported by the User Agent.")
                     } else {
-                        try {
-                            DeviceOrientationEvent.requestPermission().then(function (response) {
-                                if (response === "granted") {
-                                    sensor.addEventListener("reading", () => initSensor(orientation, sensor, initialOrientation))
-                                    sensor.start()
-                                }
-                            })
-                        } catch (err) {
-                            console.error(err)
-                        }
+                        console.log(error)
                     }
-                } else if (event.error.name === "NotReadableError") {
-                    console.log("Cannot connect to the sensor.")
                 }
-            })
-            sensor.addEventListener("reading", () => initSensor(orientation, sensor, initialOrientation))
-            sensor.start()
-        } catch (error) {
-            // Handle construction errors.
-            if (error.name === "SecurityError") {
-                // See the note above about feature policy.
-                console.log("Sensor construction was blocked by a feature policy.")
-            } else if (error.name === "ReferenceError") {
-                console.log("Sensor is not supported by the User Agent.")
             } else {
-                throw error
+                console.log("Wasn't able to get permission to the sensor.")
             }
-        }
+        })
+
         return function () {
             sensor.stop()
             sensor.removeEventListener("reading", () => initSensor(orientation, sensor, initialOrientation))
