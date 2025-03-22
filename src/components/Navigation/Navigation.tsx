40a7136cd7c86332384bec70a1b6f82bc2a841d7
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2025-03-22 19:35:33 +0100

    Update Navigation.tsx

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -12,21 +12,27 @@ import Logo from "./Logo"
 import NavigationItem from "./NavigationItem"
 import { NavigationLink } from "./types"
 
+const BASE_URL = import.meta.env.BASE_URL || "/"
+
 const NAVIGATION_LINKS: NavigationLink[] = [
     { to: "/projects", label: "Projects", isExternal: false },
     { to: "/blog", label: "Blog", isExternal: false },
-    { to: "/onepager.pdf", label: "Onepager", isExternal: true },
+    {
+        to: `${BASE_URL}onepager.pdf`.replace("//", "/"),
+        label: "Onepager",
+        isExternal: true,
+    },
 ]
 
 export default function Navigation() {
     return (
-        <Popover className="relative z-50 bg-white font-fancy">
+        <Popover className="font-fancy relative z-50 bg-white">
             <div className="flex items-center justify-between px-4 py-6 sm:px-6 md:justify-start">
                 <div className="flex justify-start lg:w-0 lg:flex-1">
                     <Logo />
                 </div>
                 <div className="-my-2 -mr-2 md:hidden">
-                    <PopoverButton className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black">
+                    <PopoverButton className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:ring-2 focus:ring-black focus:outline-none focus:ring-inset">
                         <span className="sr-only">Open menu</span>
                         <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                     </PopoverButton>
@@ -58,12 +64,12 @@ export default function Navigation() {
                     focus
                     className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
                 >
-                    <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                    <div className="ring-opacity-5 divide-y-2 divide-gray-50 rounded-lg bg-white ring-1 shadow-lg ring-black">
                         <div className="px-5 pt-5 pb-6">
                             <div className="flex items-center justify-between">
                                 <Logo />
                                 <div className="-mr-2">
-                                    <PopoverButton className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-black">
+                                    <PopoverButton className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:ring-2 focus:ring-black focus:outline-none focus:ring-inset">
                                         <span className="sr-only">
                                             Close menu
                                         </span>
@@ -75,7 +81,7 @@ export default function Navigation() {
                                 </div>
                             </div>
                         </div>
-                        <div className="py-6 px-5">
+                        <div className="px-5 py-6">
                             <div className="grid grid-cols-1 gap-4">
                                 {NAVIGATION_LINKS.map((link) => (
                                     <NavigationItem
