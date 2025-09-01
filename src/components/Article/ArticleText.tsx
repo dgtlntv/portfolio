dacf1eb50700153ec94c80a16c4084410c84722c
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2025-09-01 16:23:58 +0200

    convert some more static components

--- src/components/Article/ArticleText.tsx
+++
@@ -1,9 +0,0 @@
-import { ArticleTextProps } from "./types"
-
-export default function ArticleText({ children }: ArticleTextProps) {
-    return (
-        <div className="prose prose-lg prose-red mx-auto mt-16 marker:text-red-500">
-            {children}
-        </div>
-    )
-}
