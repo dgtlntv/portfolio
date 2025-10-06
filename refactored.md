8a654529a1518c9a93ec3322efbedabf7fcfa41b
Maximilian Blazek <68336485+dgtlntv@users.noreply.github.com>
2025-10-06 23:03:51 +0200

    refactor layout

---
+++ refactored.md
@@ -0,0 +1,15 @@
+- src/pages/blog/[slug].astro
+- src/pages/blog/index.astro
+- src/pages/projects/[slug].astro
+- src/pages/projects/index.astro
+- src/layouts/Layout.astro
+- src/components/Content/Blog/BlogContent.astro
+- src/components/Content/Blog/BlogPostsList.astro
+- src/components/Content/Blog/BlogPostsListItem.astro
+- src/components/Content/Project/ProjectContent.astro
+- src/components/Content/Project/ProjectsList.astro
+- src/components/Content/Project/ProjectsListItem.astro
+- src/pages/index.astro
+- src/styles/global.css
+- src/styles/styleSheet.ts
+- src/content/\*
--- src/components/mdxComponents.ts
+++ src/components/mdxComponents.ts
@@ -1,15 +1,15 @@
 // Import Astro components (static)
-import FloatImage from "./Article/FloatImage.astro";
-import FanImages from "./Article/FanImages.astro";
-import DocumentSkeleton from "./DocumentSkeleton.astro";
-import ColorPaletteMockup from "./ColorPaletteMockup.astro";
-import MetricsMockup from "./MetricsMockup.astro";
-import TalkExplanationAnimation from "./Talk/TalkExplanationAnimation.astro";
-import TalkHeroAnimation from "./Talk/TalkHeroAnimation.astro";
+import FloatImage from "./Article/FloatImage.astro"
+import FanImages from "./Article/FanImages.astro"
+import DocumentSkeleton from "./DocumentSkeleton.astro"
+import ColorPaletteMockup from "./ColorPaletteMockup.astro"
+import MetricsMockup from "./MetricsMockup.astro"
+import TalkExplanationAnimation from "./Talk/TalkExplanationAnimation.astro"
+import TalkHeroAnimation from "./Talk/TalkHeroAnimation.astro"
 
-// HTML element overrides with Tailwind styling
+// TODO: replace the custom html components with the ones from react
+
 export const components = {
-    // Custom components - Static (Astro)
     FloatImage,
     FanImages,
     DocumentSkeleton,
@@ -18,21 +18,17 @@ export const components = {
     TalkExplanationAnimation,
     TalkHeroAnimation,
 
-    // GitHubCodeExplorer: (props) => <GitHubCodeExplorer {...props} client:load />,
-    // TagList: (props) => <TagList {...props} client:load />,
-
-    // HTML element overrides - simplified
-    h1: 'h1',
-    h2: 'h2', 
-    h3: 'h3',
-    h4: 'h4',
-    h5: 'h5',
-    h6: 'h6',
-    ul: 'ul',
-    ol: 'ol',
-    li: 'li',
-    blockquote: 'blockquote',
-    pre: 'pre',
-    code: 'code',
-    img: 'img',
-};
+    h1: "h1",
+    h2: "h2",
+    h3: "h3",
+    h4: "h4",
+    h5: "h5",
+    h6: "h6",
+    ul: "ul",
+    ol: "ol",
+    li: "li",
+    blockquote: "blockquote",
+    pre: "pre",
+    code: "code",
+    img: "img",
+}
